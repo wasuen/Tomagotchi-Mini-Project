894e05f576601303ce8b2270daa551d62346153c
Austin <wasuen@gmail.com>
2019-07-02 13:06:15 -0700

    edit class properties

--- js/tomagotchi.js
+++ js/tomagotchi.js
@@ -1,8 +1,8 @@
 class Tomagotchi {
     constructor(name){
         this.name = name;
-        this.hunger = hunger;
-        this.sleepiness = this.sleepiness;
-        this.boredom = boredom;
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
     }
 }
