8aac0775c955d92ca5f0a030f6b15a0a8e8ebe4d
Austin <wasuen@gmail.com>
2019-07-02 12:59:20 -0700

    update master with branch

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('.start').on('click', (e) => {
+    console.log('this button is working')
+})
+
+const game = {
+    
+}
---
+++ js/tomagotchi.js
@@ -0,0 +1,8 @@
+class Tomagotchi {
+    constructor(name){
+        this.name = name;
+        this.hunger = hunger;
+        this.sleepiness = this.sleepiness;
+        this.boredom = boredom;
+    }
+}
